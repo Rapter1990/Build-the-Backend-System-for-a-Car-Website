1f55387c4b076a47ea44a782e6b1846569b47016
Noyan <sngermiyanoglu@hotmail.com>
2020-09-23 00:13:41 +0300

    Add an additional test to check whether the application appropriately generates a price for a given vehicle ID

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,39 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+// Integration Test for Pricing Service
 public class PricingServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@LocalServerPort
+	private int port;
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void getPrice() {
+		ResponseEntity<Price> response =
+				this.restTemplate.getForEntity("http://localhost:" + port + "/services/price?vehicleId=1", Price.class);
+
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+	}
 }
