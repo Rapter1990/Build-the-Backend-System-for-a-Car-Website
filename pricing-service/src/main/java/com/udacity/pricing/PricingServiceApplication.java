4a188905b7b4f97402ea2467826f6494a66bd392
Noyan <sngermiyanoglu@hotmail.com>
2020-09-24 10:58:12 +0300

    Run Eureka Client and check request path in eureka client.

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -2,14 +2,17 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
+ * TODO: Convert the application from a REST API to a microservice. (DONE)
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingServiceApplication {
 
+    // http://192.168.1.104:1001/services/price?vehicleId=1
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
     }
