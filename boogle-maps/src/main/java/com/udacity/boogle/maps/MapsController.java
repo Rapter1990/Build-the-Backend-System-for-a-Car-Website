619c80ac65fc64a5ed1627a4be416b9cbad95385
Noyan <sngermiyanoglu@hotmail.com>
2020-09-24 10:51:58 +0300

    Run Boogle Maps with http://localhost:9191/maps/?lat=20.0&lon=30.0

--- boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
+++ boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/maps")
 public class MapsController {
 
+    // http://localhost:9191/maps/?lat=20.0&lon=30.0
     @GetMapping
     public Address get(@RequestParam Double lat, @RequestParam Double lon) {
         return MockAddressRepository.getRandom();
